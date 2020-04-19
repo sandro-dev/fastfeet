7c93007540561f5a50a368a4639116130b76e2fd
Sandro Santos <sandro.dev@gmail.com>
2020-04-19 12:46:04 -0300

    [mobile] redirect after insert a problem

--- mobile/src/pages/ReportProblem/index.js
+++ mobile/src/pages/ReportProblem/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useRoute } from '@react-navigation/native';
+import { useRoute, useNavigation } from '@react-navigation/native';
 import { Alert } from 'react-native';
 import api from '~/services/api';
 
@@ -13,6 +13,7 @@ import {
 } from './styles';
 
 export default function SignIn() {
+  const navigation = useNavigation();
   const route = useRoute();
   const { delivery } = route.params;
   const [description, setDescription] = useState('');
@@ -20,7 +21,21 @@ export default function SignIn() {
   async function handleSubmit() {
     console.tron.log(description);
     try {
-      await api.post(`/delivery/${delivery.id}/problems`, { description });
+      const response = await api.post(`/delivery/${delivery.id}/problems`, { description });
+      console.tron.log(response);
+      if (response.data.id) {
+        Alert.alert(
+          'Ok!',
+          'O problema foi relatado'
+        );
+        navigation.navigate('Deliveries');
+      } else {
+        Alert.alert(
+          'Houve uma falha',
+          'Não foi possível relatar o problema a entrega. Tente novamente.'
+        );
+      }
+
     } catch (err) {
       Alert.alert('Não foi possível relatar o problema');
     }
